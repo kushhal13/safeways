95d02463839dee656d1dedfdf007238cc71e9485
kushal <kushal.chaulagain@fintechnepal.com>
2019-08-25 00:06:40 +0545

    image picker

--- lib/reportPage.dart
+++ lib/reportPage.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ReportPage extends StatefulWidget {
   @override
@@ -8,12 +11,57 @@ class ReportPage extends StatefulWidget {
 class _ReportPageState extends State<ReportPage> {
   var _dead = ["Yes", "Injured"];
   var currentItemSelected = "Yes";
-// // Changes the selected value on 'onChanged' click on each radio button
-//   setSelectedRadio(int val) {
-//     setState(() {
-//       selectedRadio = val;
-//     });
-//   }
+  var dailog;
+  File newImage;
+
+  Future optionsDialogBox() {
+    return dailog != null
+        ? Text("jfdkla")
+        : showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                content: new SingleChildScrollView(
+                  child: new ListBody(
+                    children: <Widget>[
+                      GestureDetector(
+                          child: new Text('Take a picture'),
+                          onTap: () {
+                            setState(() {
+                              dailog = "skdjf";
+                            });
+                            getImage(true);
+                          }),
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                      ),
+                      GestureDetector(
+                        child: new Text('Select from gallery'),
+                        onTap: () {
+                          setState(() {
+                            dailog = "skdjf";
+                          });
+                          getImage(false);
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            });
+  }
+
+  Future getImage(bool isCamera) async {
+    File image;
+    if (isCamera) {
+      image = await ImagePicker.pickImage(source: ImageSource.camera);
+    } else {
+      image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    }
+    setState(() {
+      newImage = image;
+    });
+  }
 
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,6 +70,37 @@ class _ReportPageState extends State<ReportPage> {
       ),
       body: Column(
         children: <Widget>[
+          newImage != null
+              ? Container(
+                  margin: const EdgeInsets.only(top: 16.0),
+                  width: 150.0,
+                  height: 150.0,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        fit: BoxFit.fill,
+                        image: FileImage(newImage),
+                      )))
+              : GestureDetector(
+                  onTap: () {
+                    optionsDialogBox();
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.only(top: 16.0),
+                    child: Center(
+                        child: Icon(
+                      Icons.add_a_photo,
+                      color: Colors.white,
+                      size: 50.0,
+                    )),
+                    width: 150.0,
+                    height: 150.0,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
           Row(
             children: <Widget>[
               Padding(
@@ -86,7 +165,7 @@ class _ReportPageState extends State<ReportPage> {
                 padding:
                     const EdgeInsets.symmetric(horizontal: 8.0, vertical: 32.0),
                 child: Text(
-                  "No.of animals",
+                  "Dead?",
                   style: TextStyle(
                     fontSize: 20.0,
                     fontWeight: FontWeight.bold,
@@ -120,11 +199,28 @@ class _ReportPageState extends State<ReportPage> {
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Text(
-                  "Dead?",
+                  "No.of \nanimals",
                   style: TextStyle(
                     fontSize: 20.0,
                     fontWeight: FontWeight.bold,
                   ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 4.0),
+                height: 50.0,
+                width: 90.0,
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: const BorderRadius.all(
+                          const Radius.circular(10.0),
+                        ),
+                      ),
+                      filled: true,
+                      hintStyle: TextStyle(color: Colors.grey[800]),
+                      hintText: "Total no.",
+                      fillColor: Colors.white70),
                 ),
               ),
             ],
